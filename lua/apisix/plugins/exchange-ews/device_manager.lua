06218f158dc70a4739b4e78002ec643b147117da
hartnett <x@xsec.io>
2019-11-29 14:47:52 +0800

    [#] add debug log

--- lua/apisix/plugins/exchange-ews/device_manager.lua
+++ lua/apisix/plugins/exchange-ews/device_manager.lua
@@ -76,7 +76,7 @@ local function get_ews_address_status(username, ip)
         if next(res) then
             if type(res[1]) == "string" then
                 local ews_info = core.json.decode(res[1])
-                core.log(string.format("ews_info: %s, ews_ip_state: %s",
+                core.log.warn(string.format("ews_info: %s, ews_ip_state: %s",
                         ews_info, tonumber(ews_info["state"])))
                 if tonumber(ews_info["state"]) < 0 then
                     result = false
